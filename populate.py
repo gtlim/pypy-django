9420a63af98700b5aa0a83855b5c1ae4f1189c9c
Yann Malet <yann.malet@gmail.com>
2015-11-25 13:57:06 +0100

    Fix an import path

--- populate.py
+++ populate.py
@@ -1,13 +1,13 @@
-
 import os
+from datetime import timedelta
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_perf_project.settings')
 import django
 django.setup()
 
 from django.utils import timezone
-from ormperf.models import Question, Choice
-from datetime import datetime, timedelta
+
+from django_perf_project.apps.ormperf.models import Question, Choice
 
 if __name__ == '__main__':
     d0 = timezone.datetime(2015, 10, 21, tzinfo=timezone.utc)
